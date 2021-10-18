899becc1b0c0c16976384c688c5a635c65f0bc5b
Aakriti <aakritig28@gmail.com>
2021-10-18 21:05:28 +0530

    chat-app-chitkara

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const socketio = require('socket.io');
+const io = socketio(server);
+const path = require('path');
+
+
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+const users={};
+
+io.on ('connection', (socket)=>{
+    console.log(`Connection estblished with ${socket.id}`);
+    socket.on('send-msg', (data)=>{
+        io.emit('recieved-msg', {
+            msg: data.msg,
+            user: users[socket.id]
+        });
+    });
+
+    socket.on('login', (data)=>{
+        users[socket.id] = data.user;
+    });
+});
+
+
+const port = process.env.PORT || 3000;
+
+
+server.listen(port, ()=>{
+    console.log(`Server listening at port ${port}`);
+});
